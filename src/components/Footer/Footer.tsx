7f1d02385f3a92983013012c732714ea36c39bf2
Alvinlo221110546 <221110546@students.mikroskil.ac.id>
2025-10-06 15:36:48 +0700

    webauth

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './styles.css'
+
+function Footer(): React.JSX.Element {
+  return (
+    <div className="f f-wrap j-center ai-center ph-1 ns footer">
+      <span>
+        <a
+          href="https://github.com/peterdee/webauthn-demo"
+          target="_blank"
+        >
+          WebAuthn demo
+        </a>
+      </span>
+      <span className="ml-half">
+        by
+      </span>
+      <span className="ml-half">
+        <a
+          href="https://github.com/peterdee"
+          target="_blank"
+        >
+          ALVIN
+        </a>
+      </span>
+      <span className="ml-half">
+        ©
+      </span>
+      <span className="ml-half">
+        { new Date().getFullYear() }
+      </span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import ssl from '@vitejs/plugin-basic-ssl'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    ssl()
+  ],
+  server: {
+    host: true,
+    port: 3000,
+  }
+})
